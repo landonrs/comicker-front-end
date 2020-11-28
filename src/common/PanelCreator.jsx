4f40bf7b3d3a89cb0b46002ea1388c863e9dd322
Landon Shumway <landon.shumway@familysearch.org>
2020-11-28 08:58:07 -0600

    Add comic toolbar for creating panels
    implemented the pen, eraser and undo functionality.
    Will need to figure out sizing for all of these...

--- src/common/PanelCreator.jsx
+++ src/common/PanelCreator.jsx
@@ -1,43 +1,99 @@
 import React, { useState } from "react";
 import { SketchPicker } from "react-color";
 import CanvasDraw from "react-canvas-draw";
-import { Slider } from "@material-ui/core";
+import { Box, IconButton, Slider, Toolbar } from "@material-ui/core";
 import { makeStyles } from "@material-ui/core/styles";
+import CreateIcon from "@material-ui/icons/Create";
+import ChatBubbleOutlineIcon from "@material-ui/icons/ChatBubbleOutline";
+import TextFieldsIcon from "@material-ui/icons/TextFields";
+import UndoIcon from "@material-ui/icons/Undo";
+import { BiEraser } from "react-icons/bi";
+
+const WHITE = "#FFFFFF";
 
 const useStyles = makeStyles({
+  panelRoot: { marginTop: "10%" },
   slider: {
-    width: "30%",
+    width: "90px",
+  },
+  canvasBorder: {
+    display: "inline-block",
   },
 });
 
+const ToolButton = (props) => {
+  const { icon, onClick } = props;
+
+  return (
+    <IconButton
+      onClick={() => onClick()}
+      color="primary"
+      aria-label="upload picture"
+      component="span"
+    >
+      {icon}
+    </IconButton>
+  );
+};
+
 const PanelCreator = (props) => {
-  const [selectedColor, setSelectedColor] = useState("#444");
+  const [penSelectedColor, setPenSelectedColor] = useState("#444");
+  const [eraserSelected, setEraserSelected] = useState(false);
+
   const [selectedBrushRadius, setSelectedBrushRadius] = useState(5);
   const classes = useStyles();
 
+  const [canvasRef, setCanvasRef] = useState(null);
+
   const onColorChange = (event) => {
-    setSelectedColor(event.hex);
+    setPenSelectedColor(event.hex);
+  };
+
+  const onPenSelected = (event) => {
+    setEraserSelected(false);
+  };
+
+  const onEraserSelected = (event) => {
+    setEraserSelected(true);
+  };
+
+  const onUndo = (event) => {
+    canvasRef.undo();
   };
 
   return (
-    <div>
-      <SketchPicker color={selectedColor} onChangeComplete={onColorChange} />
-      <Slider
-        className={classes.slider}
-        defaultValue={selectedBrushRadius}
-        aria-labelledby="brush-radius-slider"
-        valueLabelDisplay="auto"
-        step={1}
-        marks
-        min={1}
-        max={50}
-        onChange={(event, value) => setSelectedBrushRadius(value)}
-      />
-      <CanvasDraw
-        hideGrid={true}
-        brushColor={selectedColor}
-        brushRadius={selectedBrushRadius}
-      />
+    <div className={classes.panelRoot}>
+      <Toolbar>
+        <Slider
+          className={classes.slider}
+          defaultValue={selectedBrushRadius}
+          aria-labelledby="brush-radius-slider"
+          valueLabelDisplay="auto"
+          step={1}
+          marks
+          min={1}
+          max={50}
+          onChange={(event, value) => setSelectedBrushRadius(value)}
+        />
+        <ToolButton icon={<CreateIcon />} onClick={onPenSelected} />
+        <ToolButton icon={<BiEraser />} onClick={onEraserSelected} />
+        <ToolButton icon={<TextFieldsIcon />} onClick={() => {}} />
+        <ToolButton icon={<ChatBubbleOutlineIcon />} onClick={() => {}} />
+        <ToolButton icon={<UndoIcon />} onClick={onUndo} />
+      </Toolbar>
+      {/* <SketchPicker color={selectedColor} onChangeComplete={onColorChange} /> */}
+      <Box className={classes.canvasBorder} disableGutters={true} border={5}>
+        <CanvasDraw
+          ref={(canvasDraw) => setCanvasRef(canvasDraw)}
+          hideGrid={true}
+          brushColor={eraserSelected ? WHITE : penSelectedColor}
+          brushRadius={selectedBrushRadius}
+          canvasWidth={300}
+          canvasHeight={300}
+          lazyRadius={0}
+          hideInterface={true}
+        />
+      </Box>
     </div>
   );
 };
